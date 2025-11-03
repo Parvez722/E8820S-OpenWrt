8da0f7437725fc0e8cbb75d4dbfc4db73e8cc9c5
TwoOnefour <lys214412@gmail.com>
2025-11-04 01:10:47 +0800

    Update diy-part2.sh

--- script/diy-part2.sh
+++ script/diy-part2.sh
@@ -15,14 +15,8 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 # 修改主机名字，把E8820S修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/E8820S/g' package/base-files/files/bin/config_generate
 
-# 修改开源驱动wifi名称
-sed -i 's/OpenWrt_2G/5202_genshin_impact/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/OpenWrt_5G/5202_5g/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# 修改banne文件（banne文件在根目录）（不要修改此行代码,怕弄的diy-lede.sh文件全失效,不需要的话前面加#，或者全行代码删除了）
-#rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./banner openwrt/package/base-files/files/etc/ && cd openwrt
 
-# 更改时区
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #修正连接数
